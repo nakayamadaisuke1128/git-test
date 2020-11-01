b01e001f0e5e8ff9b304d16b7c2a69bed195bce6
nakayamadaisuke1128 <nd00001128@gmail.com>
2020-11-02 02:03:23 +0900

    test

--- lists/chap01/list01-02/Main.java
+++ lists/chap01/list01-02/Main.java
@@ -1,13 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         String s1 = "Java and JavaScript";
+        //一部に文字列Javaを含めるかを調べる
         if(s1.contains("Java")) {
             System.out.println("文字列s1は、Javaを含んでいます");
         }
+        //文字列Javaで終わるかを調べる
         if(s1.endsWith("Java")) {
             System.out.println("文字列s1は、Javaが末尾にあります");
         }
+        //文字列Javaが最初に登場する位置を調べる
         System.out.println("文字列s1で最初にJavaが登場する位置は" + s1.indexOf("Java"));
+        //文字列Javaを後ろから検索して最初に登場する位置を調べる
         System.out.println("文字列s1で最後にJavaが登場する位置は" + s1.lastIndexOf("Java"));
     }
 }
